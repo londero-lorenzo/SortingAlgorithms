8f0dfd3bccb4dacfd3bc8e3631d758636aaffe71
Lorenzo Londero <londero.lorenzo.sc@gmail.com>
2025-05-24 18:37:49 +0200

    add global variables and utility functions to setup.py
    - locate python and jupyter lab paths for different os
    - install notebook strip filter
    - used by: create_sample_arrays, start_jupyter, start_venv

--- setup.py
+++ setup.py
@@ -23,7 +23,25 @@ def get_pip_path():
     else:
         return os.path.join(VENV_DIR, "bin", "pip")
 
-
+def get_jupyter_path():
+    if os.name == 'nt':
+        return os.path.join(VENV_DIR, "Scripts", "jupyter.exe")
+    else:
+        return os.path.join(VENV_DIR, "bin", "jupyter")
+        
+        
+def get_activate_path():
+    if os.name == 'nt':
+        return os.path.join(VENV_DIR, 'Scripts', 'activate.bat')
+    else:
+        return os.path.join(VENV_DIR, 'bin', 'activate')
+    
+    
+def get_python_path():
+    if os.name == 'nt':
+        return os.path.join(VENV_DIR, 'Scripts', 'python.exe')
+    else:
+        return os.path.join(VENV_DIR, 'bin', 'python')
 
 def install_requirements():
     pip_path = get_pip_path()
@@ -34,19 +52,52 @@ def install_requirements():
 
     print("Installing packages from requirements.txt...")
     subprocess.check_call([pip_path, "install", "-r", REQUIREMENTS_FILE])
+   
+
+def install_filters():
+    print("Installing git filters...")
+    activate_path = get_activate_path()
+    if not os.path.isfile(activate_path):
+        print(f"ERROR: activation executable not found at {activate_path}")
+        sys.exit(1)
+
+
+    if os.name == 'nt':
+        clean_command = f'cmd.exe /c "{activate_path} && python {os.path.abspath("strip_notebook_filter.py")}"'
+
+        subprocess.check_call([
+        "git", "config", "filter.strip-notebook.clean",
+        clean_command
+        ])
+        
+    else:
+    
+        clean_command = f'bash -c "source {activate_path} && jupyter  nbconvert --ClearOutputPreprocessor.enabled=True --ClearMetadataPreprocessor.enabled=True --to notebook --stdout"'
+        subprocess.check_call([
+        "git", "config", "filter.strip-notebook.clean",
+        clean_command
+        ])
+    subprocess.check_call(["git", "config", "filter.strip-notebook.smudge", 'cat'])
+        
     
     
 def main():
-    create_virtualenv()
+    if os.path.isdir(VENV_DIR):
+        print(f"Virtual environment already exists in {VENV_DIR}")
+    else:
+        create_virtualenv()
     install_requirements()
+    install_filters()
     print("Setup completed.")
     print(f"To activate the environment manually:\n")
 
     if os.name == 'nt':
-        print(f"   .\\{VENV_DIR}\\Scripts\\activate.bat")
+        print(f" {VENV_DIR}\\Scripts\\activate.bat")
     else:
-        print(f"   source {VENV_DIR}/bin/activate")
-
+        print(f" source {VENV_DIR}/bin/activate")
+    
+    print()
+    input("Press ENTER to quit.")
 
 if __name__ == "__main__":
     main()
